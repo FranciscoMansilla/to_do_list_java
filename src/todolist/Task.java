e420e6f64f3880ebff9576a15751fb46878bcf8d
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2023-06-09 17:11:50 -0300

    first commit

---
+++ src/todolist/Task.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package todolist;
+
+/**
+ *
+ * @author FRAN999
+ */
+public class Task {
+    private String title;
+    private String description;
+    private int status;
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.status = 0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
